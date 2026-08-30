6c2b585cf1eb1c1330ba3b50557213b59a4dd120
Zagorka <anevskaz17@gmail.com>
2026-08-30 16:22:57 +0200

    Swagger documentation

---
+++ food-ordering-system/restaurant-service/src/main/kotlin/mk/finki/restaurantservice/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package mk.finki.restaurantservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/actuator/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest().permitAll()
+            }
+        return http.build()
+    }
+}
